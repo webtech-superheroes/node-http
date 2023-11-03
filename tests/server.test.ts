93cda4a34021e436189f9b7da7aec17d528aa30b
Eduard Budacu <eduard.budacu@gmail.com>
2023-11-03 10:43:51 +0100

    added 404 test

--- tests/server.test.ts
+++ tests/server.test.ts
@@ -2,41 +2,46 @@ import server from '../src/server';
 import http from 'http';
 import supertest from 'supertest';
 
-describe("test add function",  () => {
-    it("should have a valid server",() => {
+describe("test add function", () => {
+    it("should create a valid server", () => {
         expect(server).toBeInstanceOf(http.Server);
-        
+    });
+
+    it("should respond with 404 for non existing endpont",async () => {
+        const response = await supertest(server)
+            .get('/not-available')
+            .expect(404);
     });
 
     it("should respond with messages", async () => {
-        const test = await supertest(server)
+        const response = await supertest(server)
             .get('/messages')
             .expect(200);
-        expect(test.body).toBeDefined();
-        expect(test.body.messages.length).toBe(0);
-    })
+        expect(response.body).toBeDefined();
+        expect(response.body.messages.length).toBe(0);
+    });
 
     it("should accept messages", async () => {
-        const test = await supertest(server)
+        const response = await supertest(server)
             .post('/messages')
             .send({ message: 'Hello, World!' })
             .set('Accept', 'application/json')
             .expect(201);
-    })
+    });
 
     it("should have one message", async () => {
-        const test = await supertest(server)
+        const response = await supertest(server)
             .get('/messages')
             .expect(200);
-        expect(test.body).toBeDefined();
-        expect(test.body.messages.length).toBe(1);
-    })
+        expect(response.body).toBeDefined();
+        expect(response.body.messages.length).toBe(1);
+    });
 
     it("should throw error when message not provided", async () => {
-        const test = await supertest(server)
+        const response = await supertest(server)
             .post('/messages')
             .send({ msg: 'Hello, World!' })
             .set('Accept', 'application/json')
             .expect(400);
-    })
+    });
 });
